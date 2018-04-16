9e2b7252e16258b0e63b32e8a63f99ecd359c21e
rynxiao <yuzhongzi91@sina.com>
2018-04-16 15:55:28 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,58 @@
+var Config = (function() {
+    var albumList = [
+        {
+            name: '141213_Blooming.jpg',
+            description: 'xxx',
+            date: 'xxx',
+            en: 'xxx'
+        },
+        {
+            name: '141214_The-first-time.jpg.jpg',
+            description: 'xxx',
+            date: 'xxx',
+            en: 'xxx'
+        },
+        {
+            name: '150322_Fan.jpg',
+            description: 'xxx',
+            date: 'xxx',
+            en: 'xxx'
+        },
+        {
+            name: '150326_Plaisir.jpg',
+            description: 'xxx',
+            date: 'xxx',
+            en: 'xxx'
+        },
+        {
+            name: '150330_Spring-Angle.jpg',
+            description: 'xxx',
+            date: 'xxx',
+            en: 'xxx'
+        },
+        {
+            name: '150403_Golden-box.jpg',
+            description: 'xxx',
+            date: 'xxx',
+            en: 'xxx'
+        },
+        {
+            name: '150411_Sister.jpg',
+            description: 'xxx',
+            date: 'xxx',
+            en: 'xxx'
+        },
+        {
+            name: '150413_Rain-in-the-sun.jpg',
+            description: 'xxx',
+            date: 'xxx',
+            en: 'xxx'
+        },
+        {
+            name: '150512_Petit-cadeau.jpg',
+            description: 'xxx',
+            date: 'xxx',
+            en: 'xxx'
+        },
+    ];
+})();
